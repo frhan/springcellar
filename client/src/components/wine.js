ecc94f158d86c73b4b2df762d66c2aa5f3b506a3
Farhan Faruque <russel.cse05@gmail.com>
2018-02-08 17:45:34 +0600

    Wine fetching complete

--- client/src/components/wine.js
+++ client/src/components/wine.js
@@ -1,37 +1,49 @@
 import React from 'react';
-import "./wine.css"
-import {Button,ButtonToolbar} from "react-bootstrap";
-// import './bootstrap/css/bootstrap.css';
-//
-export default class Wine extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import "./wine.css";
+import 'whatwg-fetch';
 
-  render(){
-    return(
-      <div>
-      <div className="col-md-2 column productbox">
-    <img src="http://placehold.it/460x250/e67e22/ffffff&text=HTML5" className="img-responsive"/>
-    <div className="producttitle">Product 2</div>
-    <div className="productprice"><div className="pull-right"><a href="#" className="btn btn-danger btn-sm" role="button">BUY</a></div><div className="pricetext">£8.95</div></div>
-</div>
-<div className="col-md-2 column productbox">
-    <img src="http://placehold.it/460x250/e67e22/ffffff&text=HTML5" className="img-responsive"/>
-    <div className="producttitle">Product 2</div>
-    <div className="productprice"><div className="pull-right"><a href="#" className="btn btn-danger btn-sm" role="button">BUY</a></div><div className="pricetext">£8.95</div></div>
-</div>
-<div className="col-md-2 column productbox">
-    <img src="http://placehold.it/460x250/e67e22/ffffff&text=HTML5" className="img-responsive"/>
-    <div className="producttitle">Product 3</div>
-    <div className="productprice"><div className="pull-right"><a href="#" className="btn btn-danger btn-sm" role="button">BUY</a></div><div className="pricetext">£8.95</div></div>
-</div>
-<div className="col-md-2 column productbox">
-    <img src="http://placehold.it/460x250/e67e22/ffffff&text=HTML5" className="img-responsive"/>
-    <div className="producttitle">Product 4</div>
-    <div className="productprice"><div className="pull-right"><a href="#" className="btn btn-danger btn-sm" role="button">BUY</a></div><div className="pricetext">£8.95</div></div>
-</div>
-    </div>
+
+export default class Wine extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            photos: []
+        };
+    }
+
+    componentWillMount() {
+        this.fetchPhotos();
+    }
+
+    async fetchPhotos() {
+        let response = await fetch("http://www.mocky.io/v2/5a7c37812e00004700052816").then((resp) => resp.json());
+        this.setState({
+            photos: response
+        });
+    }
+
+    render() {
+        return (
+            <ItemList items={this.state.photos}/>
+        );
+    }
+}
+
+function ItemList({items}) {
+    return (
+        <div>
+            { items.map(item =>
+                <div className="col-md-2 column productbox" key={item.id}>
+                    <img src={item.image} className="img-responsive" alt="#"/>
+                    <div className="producttitle">{item.name}</div>
+                    <div className="productprice">
+                        <div className="pull-right"><a href="#" className="btn btn-danger btn-sm" role="button">BUY</a>
+                        </div>
+                        <div className="pricetext">{item.price}</div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
-  }
 }
